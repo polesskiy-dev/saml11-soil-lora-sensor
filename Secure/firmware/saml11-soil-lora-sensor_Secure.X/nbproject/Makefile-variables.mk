dca61dfccd1837557ff5124c9803c23f64ba9e31
apolisskyi <apolisskyi@n-ix.com>
2022-08-22 15:46:01 +0300

    Initial commit

---
+++ Secure/firmware/saml11-soil-lora-sensor_Secure.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=saml11-soil-lora-sensor_Secure.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/saml11-soil-lora-sensor_Secure.X.production.hex
